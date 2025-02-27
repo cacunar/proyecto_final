2397439580715c1723f35a879a01b16af6030cf5
Carlos Acuna <cacuna@Carloss-MacBook-Air.local>
2025-02-27 20:46:23 -0300

    add seguridad

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,22 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes/routes");
+const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: [
-        "http://localhost:3000",
-        "https://proyecto-final-sva5.onrender.com"
-      ],
-    credentials: true
-}
-));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", routes);
 
-if (process.env.NODE_ENV !== "test") {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-}
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
 
 module.exports = app;
